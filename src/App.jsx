84b71b9b2bd33299c033ae49329a2c4c2d436768
Ndimah Mike <ndimah22@protonmail.com>
2024-03-12 17:34:19 +0100

    feat(UI): implement the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import {Footer} from "./components";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn Solid
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,50 @@
+import sprites from "../assets/assets.svg?url";
+function Footer() {
+    return (
+        <footer>
+            <svg width="143" height="25">
+                <title>Audiophile Logo</title>
+                <use href={sprites + "#logo"} />
+            </svg>
+            <ul class="column" role="list">
+                <li><a href="">home</a></li>
+                <li><a href="">headphones</a></li>
+                <li><a href="">speakers</a></li>
+                <li><a href="">earphones</a></li>
+            </ul>
+            <p>
+                Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
+            </p>
+            <div className="column">
+                <p>copyright 2021. all rights reserved</p>
+                <ul class="row" role="list">
+                    <li>
+                        <a href="" aria-label="follow us on Facebook">
+                            <svg width="24" height="24">
+                                <title>Facebook Logo</title>
+                                <use href={sprites + "#facebook"} />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" aria-label="follow us on Twitter">
+                            <svg width="24" height="24">
+                                <title>Twitter Logo</title>
+                                <use href={sprites + "#twitter"} />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" aria-label="follow us on Instagram">
+                            <svg width="24" height="24">
+                                <title>Instagram Logo</title>
+                                <use href={sprites + "#instagram"} />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
