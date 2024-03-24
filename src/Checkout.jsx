a8e955dac12941dd14b496c74f329daec0385e2b
Ndimah Mike <ndimah22@protonmail.com>
2024-03-24 20:54:53 +0100

    feat(interactions): implement checkout form steps navigation

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,22 +1,27 @@
-import { For } from "solid-js";
+import { createSignal, For } from "solid-js";
 import { Header, NavProvider, getNavContext } from "./components";
+import icons from "./assets/icons.svg";
+import style from "./checkout.module.css";
 
 const fieldsets = [
     {
         title: "Billing details",
         inputs: [
             {
-                metadata: { "type": "text", "id": "s-name", "name": "shippingName", "autocomplete": "name", "data-new": "", "required": true },
-                "label": "name"
+                "errorMessage": "No name provided",
+                "label": "name",
+                metadata: { "type": "text", "id": "s-name", "name": "shippingName", "autocomplete": "name", "data-new": "", "required": true }
             },
             {
-                metadata: { "type": "email", "id": "s-mail", "name": "shippingEmail", "autocomplete": "email", "data-new": "", "required": true, "placeholder": "ex. john@doe.me" },
-                "label": "email address"
+                "errorMessage": "invalid email",
+                "label": "email address",
+                metadata: { "type": "email", "id": "s-mail", "name": "shippingEmail", "autocomplete": "email", "data-new": "", "required": true, "placeholder": "ex. john@doe.me" }
             },
 
             {
-                metadata: { "type": "tel", "id": "s-tel", "name": "shippingPhone", "autocomplete": "tel", "data-new": "", "required": true, "placeholder": "ex. +237649383039", "pattern": "^\+\d{1,3}\d{8,12}$" },
-                "label": "phone number"
+                "errorMessage": "Wrong format",
+                "label": "phone number",
+                metadata: { "type": "tel", "id": "s-tel", "name": "shippingPhone", "autocomplete": "tel", "data-new": "", "required": true, "placeholder": "ex. +237649383039", "pattern": "^\\+\\d{1,3}\\d{8,12}$" }
             },
 
         ]
@@ -25,37 +30,112 @@ const fieldsets = [
         title: "shipping infos",
         inputs: [
             {
-                metadata: { "type": "text", "id": "s-address", "name": "address", "autocomplete": "address-line1" },
-                "label": "your address"
+                "errorMessage": "can not be empty",
+                "label": "your address",
+                metadata: { "type": "text", "id": "s-address", "name": "address", "autocomplete": "address-line1", required: true, "data-new": "" }
             },
             {
-                metadata: { "type": "text", "id": "s-zip", "name": "postCode", "autocomplete": "postal-code" },
-                "label": "zip code"
+                "label": "zip code",
+                metadata: { "type": "text", "id": "s-zip", "name": "postCode", "autocomplete": "postal-code" }
             },
             {
-                metadata: { "type": "text", "id": "s-city", "name": "city", "autocomplete": "address-line2" },
-                "label": "city"
+                "errorMessage": "can not be empty",
+                "label": "city",
+                metadata: { "type": "text", "id": "s-city", "name": "city", "autocomplete": "address-line2", required: true, "data-new": "" }
             },
             {
-                metadata: { "type": "text", "id": "s-country", "name": "country", "autocomplete": "country-name" },
-                "label": "country"
+                "errorMessage": "can not be empty",
+                "label": "country",
+                metadata: { "type": "text", "id": "s-country", "name": "country", "autocomplete": "country-name", required: true, "data-new": "" }
             },
 
         ]
     }
 
 ];
+const steps = ["your informations", "the shipping address", "how will you pay ?"];
+
+function Indicator(props) {
+    return (
+        <ul class={style["step-indicator"]}>
+            <For each={props.steps}>
+                {
+                    (step, index) => <li {...(props.current() === index() ? {"class": style["step-active"]} : {}) }><p>{step}</p></li>
+                }
+            </For>
+        </ul>
+    );
+}
 
 function CheckoutPage() {
+    const components = Object.create(null);
+    const [currentStep, setStep] = createSignal(0);
+
+    function requestNextFormStep() {
+        const selector = "step-by-step > :not(.step-out) :is(input,select)";
+        let inputFields = Array.from(components.form.querySelectorAll(selector));
+        if (allFieldsValid(inputFields)) {
+            components.form.firstElementChild.nextStep();
+        }
+    }
+
+    function handleInputFocus({ target }) {
+        target.dataset.new = "";
+    }
+
+    function allFieldsValid(fields) {
+        return fields.every(function(field) {
+            delete field.dataset.new;
+            return field.checkValidity();
+        });
+    }
+
+    function stepUpdater({ detail }) {
+        const { current } = detail;
+        if (current > 0) {
+            components.prev.disabled = false;
+        } else {
+            components.prev.disabled = true;
+        }
+        if (current === steps.length - 1) {
+            components.next.disabled = true;
+        } else {
+            components.next.disabled = false;
+        }
+        setStep(current);
+    }
+
+    function previousStep() {
+        components.form.firstElementChild.previousStep();
+    }
+
+    function handleChange({ target }) {
+        const { form } = components;
+        if (target.name !== "paymentType") {
+            return;
+        }
+        if (form.elements.paymentType.value === "mobile") {
+            delete form.dataset.cashSelected;
+            form.dataset.mobileSelected = "";
+        } else {
+            delete form.dataset.mobileSelected;
+            form.dataset.cashSelected = "";
+        }
+    }
+
     return (
         <NavProvider>
             <Header>
-                <a href="">go back</a>
             </Header>
-            <main>
+            <main class="stack">
                 <h1>checkout</h1>
-                <form action="">
-                    <step-by-step>
+                <Indicator steps={steps} current={currentStep} />
+                <div class="segragator no-gap no-padding">
+                    <button ref={components.prev} aria-label="previous step" class="box row" id="prev_step" type="button" data-icon-position="start" data-icon="arrow_left" disabled="true" autocomplete="off" onClick={previousStep}>previous step</button>
+                    <button ref={components.next} aria-label="next step" class="box row" id="next_step" type="button" data-icon-position="end" data-icon="arrow_right" onClick={requestNextFormStep}>next step</button>
+                </div>
+                <form action="" data-mobile-selected ref={components.form} onFocusIn={handleInputFocus} onChange={handleChange}>
+                    <step-by-step on:indexupdated={stepUpdater}>
                         <For each={fieldsets}>
                             {
                                 (set) => (
@@ -66,7 +146,7 @@ function CheckoutPage() {
                                                 (entry) => (
                                                     <div>
                                                         <input {...entry.metadata} />
-                                                        <label for={entry.metadata.id}>{entry.label}</label>
+                                                        <label for={entry.metadata.id} {...{ "data-error": entry.errorMessage }}>{entry.label}</label>
                                                     </div>
                                                 )
                                             }
@@ -79,12 +159,25 @@ function CheckoutPage() {
                             <legend>payment details</legend>
                             <div class="pm-grid">
                                 <h5 class="capitalize">payment method</h5>
-                                <label class="box row"><input type="radio" id="e-money" value="mobile" name="paymentType" /> e-Money</label>
-                                <label class="box row"><input type="radio" id="cash" value="cash" name="paymentType" /> cash on delivery</label>
+                                <div class="relative">
+                                    <input type="radio" id="e-money" value="mobile" name="paymentType" checked />
+                                    <label class="box" for="e-money">e-Money</label>
+                                </div>
+                                <div class="relative">
+                                    <input type="radio" id="cash" value="cash" name="paymentType" />
+                                    <label class="box" for="cash">cash on delivery</label>
+                                </div>
                             </div>
                             <div className="column" data-payment="mobile">
-                                <div><input type="text" id="t-number" pattern="" name="transNumber"/><label htmlFor="">e-Money number</label></div>
-                                <div><input type="text" id="t-pin" pattern="\d{4,}" name="transPin"/><label htmlFor="">e-Money PIN</label></div>
+                                <div><input type="text" id="t-number" pattern="" name="transNumber" /><label htmlFor="">e-Money number</label></div>
+                                <div><input type="text" id="t-pin" pattern="\d{4,}" name="transPin" /><label htmlFor="">e-Money PIN</label></div>
+                            </div>
+                            <div className="row" data-payment="cash">
+                                <svg width="48" height="48" class="no-shrink" data-icon-theme="primary">
+                                    <title>illustration of a cash payment</title>
+                                    <use href={icons + "#cash-on-payment"} />
+                                </svg>
+                                <p>The ‘Cash on Delivery’ option enables you to pay in cash when our delivery courier arrives at your residence. Just make sure your address is correct so that your order will not be cancelled.</p>
                             </div>
                         </fieldset>
                     </step-by-step>
