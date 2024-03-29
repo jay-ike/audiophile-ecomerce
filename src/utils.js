483835dbd56664dc63dc447d37af86f0d35a6931
Ndimah Mike <ndimah22@protonmail.com>
2024-03-29 16:15:19 +0100

    feat(api): implement client-side storage and api utilities

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 /*jslint browser*/
+import {openDB} from "./idb-min.js";
+const {DOMException, Storage, btoa, console, localStorage} = window;
 
 const plural = (count) => (
     count > 1
@@ -54,15 +56,138 @@ function getFormatter() {
     });
 }
 function getTax(amount) {
-    return Number.parseFloat((0.1925 * amount).toFixed(2));
+    const result = Number.parseFloat(0.1925 * amount);
+    return result.toFixed(2);
 }
 
 function shippingCost(cartAmount) {
     return Math.round(0.05 * cartAmount);
 }
 
+
+function upserter(db, store, keyFn) {
+    return async function (value) {
+        let oldValue = await db.get(store, keyFn(value));
+        oldValue = Object.assign(oldValue ?? {}, value);
+        await db.put(store, oldValue);
+        return oldValue;
+    };
+}
+
+function fetcher(db, store) {
+    return () => db.getAll(store);
+}
+
+function storage() {
+    const store = localStorage;
+    const isSupported = typeof Storage === "function";
+    return Object.freeze({
+        empty() {
+            store.clear();
+        },
+        get(key) {
+            let value = store.getItem(key);
+            try {
+                return JSON.parse(value);
+            } catch (error) {
+                if (SyntaxError.prototype.isPrototypeOf(error)) {
+                    return value;
+                }
+            }
+        },
+        isSupported,
+        remove(key) {
+            store.removeItem(key);
+        },
+        set(key, value) {
+            try {
+                store.setItem(key, JSON.stringify(value));
+            } catch (error) {
+                if (DOMException.prototype.isPrototypeOf(error)) {
+                    console.warn(error);
+                }
+            }
+        },
+        values() {
+            return Object.assign({}, store);
+        }
+    });
+}
+
+async function createDb(dbName = "jay-ike_shop", version = 1) {
+    const product_store = "audio_products";
+    const order_store = "audio_orders";
+    const cartKey = btoa("audio_cart");
+    const cartStore = storage();
+    let result = Object.create(null);
+    const db = await openDB(dbName, version, {
+        upgrade: function upgrade(db) {
+            db.createObjectStore(
+                product_store,
+                {keyPath: "id"}
+            ).createIndex("category", "category", {unique: false});
+            db.createObjectStore(order_store, {keyPath: "timestamp"});
+        }
+    });
+    result.addMany = async function insertMany(product_store) {
+        let tx = db.transaction(product_store, "readwrite");
+        let actions = product_store.map((elt) => tx.store.add(elt));
+        actions[actions.length] = tx.done;
+        await Promise.all(actions);
+    };
+    result.upsertOrder = upserter(db, order_store, (order) => order.timestamp);
+    result.upsertProduct = upserter(db, product_store, (prod) => prod.id);
+    result.getAllProducts = fetcher(db, product_store);
+    result.getAllOrders = fetcher(db, order_store);
+    result.getProductById = (id) => db.get(product_store, id);
+    result.getProductsByCategories = async function category(categories) {
+        let request;
+        if (!Array.isArray(categories)) {
+            return [];
+        }
+        request = await db.getAllFromIndex(product_store, "status");
+        return request.filter((elt) => categories.includes(elt.status));
+    };
+    result.deleteProduct = (id) => db.delete(product_store, id);
+    result.saveCart = function (cart) {
+        cartStore.set(cartKey, cart);
+    };
+    result.getCart = function () {
+        cartStore.get(cartKey);
+    };
+    return result;
+}
+
+async function fetchData(url, options, timeout) {
+    let controller = new AbortController();
+    let response;
+    let success;
+    const defaultOptions = {
+        headers: {"content-type": "application/json"},
+        method: "GET",
+        signal: controller.signal
+    };
+    if (Number.isFinite(timeout)) {
+        setTimeout(() => controller.abort(), timeout);
+    }
+    try {
+        response = await fetch(
+            url,
+            Object.assign(defaultOptions, options ?? {})
+        );
+        success = response.ok;
+        response = await response.json();
+        return Object.assign({success}, response);
+    } catch (error) {
+        return Object.assign({success: false}, {message: error.message});
+    }
+
+}
+
 export default Object.freeze({
     copy,
+    createDb,
+    fetchData,
     getFormatter,
     getTax,
     plural,
